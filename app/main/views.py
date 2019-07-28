581365026064537499817bcf484e484a98646e27
Janice-M <janiceink001@gmail.com>
2019-07-28 20:22:36 +0300

    removed forms

--- app/main/views.py
+++ app/main/views.py
@@ -1,32 +1,24 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from ..request import get_movies,get_movie,search_movie
-from ..models import Review
-from .forms import ReviewForm
-
-
-
+from ..models import Article
 
 #views 
 @main.route('/')
 def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-    #getting popular movies
-    popular_movies = get_movies('popular')
-    upcoming_movie = get_movies('upcoming')
-    now_showing_movie = get_movies('now_playing')
-    title = 'Home - Welcome to The Best best Movie Review Website Online'
     
-    # message = 'Hello World'
+    author = get_movies('author')
+    title = get_movies('title')
+    description = get_movies('description')
+    publishedAt = get_movies('publishedAt')
+    
+    # # message = 'Hello World'
     search_movie = request.args.get('movie_query')
 
     if search_movie:
         return redirect(url_for('main.search',movie_name = search_movie))
     else:
-        return render_template('index.html',title = title, popular = popular_movies,upcoming = upcoming_movie, now_playing = now_showing_movie)
+        return render_template('index.html', author = author, title = title, description = description, publishedAt = publishedAt)
 
 @main.route('/movie/<int:id>')
 def movie(id):
@@ -34,10 +26,13 @@ def movie(id):
     View root page function theat returns the index pages and its  data 
     '''
     movie = get_movie(id)
+    author = f'{movie.author}'
     title = f'{movie.title}'
-    reviews = Review.get_reviews(movie.id)
-    return render_template('movie.html',  title = title, movie = movie, reviews = reviews)
+    description = f'{movie.description}'
+    publishedAt = f'{movie.publishedAt}'
+    return render_template('movie.html',  author = author, title = title, description = description, publishedAt = publishedAt)
 
+"""
 @main.route('/search/<movie_name>')
 def search(movie_name):
     '''
@@ -64,3 +59,4 @@ def new_review(id):
     
     title = f'{movie.title} review'
     return render_template('new_review.html', title = title, review_form = form, movie = movie)
+"""
--- app/models.py
+++ app/models.py
@@ -6,16 +6,12 @@ class Source:
     def __init__(self,id,name):
         self.id =id
         self.name = name
-
-    
-
-
-
+        
 class Article:
     
     """ class article gives more insights on the news stories """
     
-    def  __init__(self,author ,title, description, url, urlToImage , publishedAt, content ):
+    def  __init__(self, author ,title, description, url, urlToImage , publishedAt, content ):
         self.author = author
         self.title = title
         self.description = description
